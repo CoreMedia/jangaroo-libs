aace539b3aee234bb98d8ad839dbb6b753460cea
Stefan Eckhorst <Stefan.Eckhorst@coremedia.com>
2009-11-20 15:02:23 +0100

    starting point for dev
    [git-p4: depot-paths = "//coremedia/jangaroo-libs/": change = 164380]

--- ckeditor/src/main/joo-js/ckeditor/config.js
+++ ckeditor/src/main/joo-js/ckeditor/config.js
@@ -3,9 +3,10 @@ Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
-CKEDITOR.editorConfig = function( config )
+CKEDITOR.editorConfig = function(config)
 {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+        config.theme = 'cm';
 };
