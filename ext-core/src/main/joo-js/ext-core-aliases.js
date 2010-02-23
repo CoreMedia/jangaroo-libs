820971dc89650964d7d5a472d23e967a4d9077e0
Frank Wienberg <frank.wienberg@coremedia.com>
2010-02-23 09:16:08 +0100

    Ext Core needs the same ext.util.Observalble patch as Ext JS. Makes client-core's unit tests green again.
    [git-p4: depot-paths = "//coremedia/jangaroo-libs/": change = 170327]

--- ext-core/src/main/joo-js/ext-core-aliases.js
+++ ext-core/src/main/joo-js/ext-core-aliases.js
@@ -1,2 +1,8 @@
 ext = Ext;
 ext.Ext = Ext;
+// Patch upper-case-bug of ext.util.Observable:
+ext.util.Observable.prototype.hasListener = (function(oldHasListener) {
+  return function(eventName) {
+    return oldHasListener.call(this, eventName.toLowerCase());
+  };
+})(ext.util.Observable.prototype.hasListener);
