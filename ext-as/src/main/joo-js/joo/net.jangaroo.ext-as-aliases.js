bef229b753c18f8d7e0b4e72962e4d766eff61b2
Frank Wienberg <frank.wienberg@coremedia.com>
2011-02-02 16:23:07 +0100

    Changed the way to give Ext's Date prototype extensions an AS3 API back to square one: Since the Date class is final in AS3 and must be final in Jangaroo, too, in order to reuse the ASDoc tool, we cannot fake an ExtDate subclass of Date. A separate ExtDate class would not be type-compatible to Date. Thus, we (again) mock an ext.util.DateUtil class containing static methods that take the Date as their first argument. Instead of delegating every single method, net.jangaroo.ext-as-aliases.js forwards methods based on the number of parameters (to avoid generic apply calls). Thus, for example, instead of   ExtDate(getDate()).format("c") you now have to use   DateUtil.format(getDate(), "c")

--- ext-as/src/main/joo-js/joo/net.jangaroo.ext-as-aliases.js
+++ ext-as/src/main/joo-js/joo/net.jangaroo.ext-as-aliases.js
@@ -13,15 +13,30 @@ ext.data.jsonreader = Ext.data.JsonReader;
 ext.data.store = Ext.data.Store;
 ext.form.action = Ext.form.Action;
 ext.layout.borderlayout = Ext.layout.BorderLayout;
-ext.util.ExtDate = Date;
+ext.util.DateUtil = Date;
+// Forward all additional Date instance methods as static methods, since AS3 does not allow to extend a final class:
+Ext.iterate({getTimezone: 0, getDayOfYear:0, getWeekOfYear:0, isLeapYear:0, getFirstDayOfMonth:0, getLastDayOfMonth:0,
+  getFirstDateOfMonth:0, getLastDateOfMonth:0, getDaysInMonth:0, getSuffix:0, clone:0, isDST:0,
+  getGMTOffset:1, clearTime:1, add:2, between:2, format:1},
+  function(method, arity) {
+    // We could do this in a general fashion, but that would mean using apply() combined with Array#slice(),
+    // which is more runtime overhead.
+    Date[method] =
+      arity === 0 ?
+        function(date) {
+          return date[method]();
+        }
+        : arity === 1 ?
+        function(date, arg1) {
+          return date[method](arg1);
+        }
+        : // arity can only be 2
+        function(date, arg1, arg2) {
+          return date[method](arg1, arg2);
+        };
+  });
 ext.util.StringUtil = String;
 // aliases for overloaded methods, renamed in Ext AS API:
-ext.menu.Menu.prototype.showMenu = function(element, position, parentMenu) {
-  this.show(element, position, parentMenu);
-};
-ext.Component.prototype.addClasses = function(classes) {
-  return this.addClass(classes);
-};
-ext.Component.prototype.removeClasses = function(classes) {
-  return this.removeClass(classes);
-};
+ext.menu.Menu.prototype.showMenu = Ext.menu.Menu.prototype.show;
+ext.Component.prototype.addClasses = Ext.Component.prototype.addClass;
+ext.Component.prototype.removeClasses = Ext.Component.prototype.removeClass;
