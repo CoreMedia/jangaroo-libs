3e59e66e083465d26b1a845f5e1877d94ed9b20a
Frank Wienberg <frank.wienberg@coremedia.com>
2011-04-08 14:49:31 +0200

    Temporary fix for missing class XML in non-Firefox browsers

--- jooflash/src/main/joo-js/joo/jooflash.module.js
+++ jooflash/src/main/joo-js/joo/jooflash.module.js
@@ -1,2 +1,5 @@
+if (typeof XML === "undefined") {
+  XML = {}; // hack: fake XML class for browsers not supporting E4X (all but Firefox). If anywhere, belongs into Joo Runtime.
+}
 joo.loadScript("joo/net.jangaroo.jooflash-native.js");
 joo.loadModule("net.jangaroo", "jooflash");
