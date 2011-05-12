07c59278647f2cd00ac07a5d1d04646f6a3e4540
Manuel Ohlendorf <manuel.ohlendorf@coremedia.com>
2011-05-12 16:16:37 +0200

    extjs locale files added and a small script that loads them.

--- ext-js/src/main/joo-js/joo/ext-js.module.js
+++ ext-js/src/main/joo-js/joo/ext-js.module.js
@@ -1,3 +1,4 @@
 joo.loadScript("ext-js/adapter/ext/ext-base.js", "ext-js/adapter/ext/ext-base-debug.js");
 joo.loadScript("ext-js/ext-all.js", "ext-js/ext-all-debug.js");
 joo.loadDebugScript("ext-js/src/debug.js");
+joo.loadScript("loadLocale.js");
---
+++ ext-js/src/main/joo-js/loadLocale.js
@@ -0,0 +1,5 @@
+//Script that loads the exjs locale file based on the jangaroo locale setting (if available)
+if(joo.ResourceBundleAwareClassLoader
+    && joo.ResourceBundleAwareClassLoader.INSTANCE.getLocale()) {
+  joo.loadScript('ext-js/src/locale/ext-lang-' + joo.ResourceBundleAwareClassLoader.INSTANCE.getLocale() + '.js');
+}
