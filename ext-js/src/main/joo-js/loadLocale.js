cd1f98290d47b689db331e61326969468f2a3ffa
Frank Wienberg <frank.wienberg@coremedia.com>
2011-05-25 20:16:28 +0200

    Fix: be more robust, so Ext AS also works without Jangaroo localization configuration

--- ext-js/src/main/joo-js/loadLocale.js
+++ ext-js/src/main/joo-js/loadLocale.js
@@ -1,5 +1,6 @@
 //Script that loads the exjs locale file based on the jangaroo locale setting (if available)
-if(joo.ResourceBundleAwareClassLoader
+if (joo.ResourceBundleAwareClassLoader
+    && joo.ResourceBundleAwareClassLoader.INSTANCE
     && joo.ResourceBundleAwareClassLoader.INSTANCE.getLocale()) {
   joo.loadScript('ext-js/src/locale/ext-lang-' + joo.ResourceBundleAwareClassLoader.INSTANCE.getLocale() + '.js');
 }
