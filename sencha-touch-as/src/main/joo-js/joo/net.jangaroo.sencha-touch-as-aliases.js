6d1feac5e0bd4a9cee2f5f684097b7c731ce4a9d
Frank Wienberg <frank.wienberg@coremedia.com>
2011-09-02 15:54:46 +0200

    Adding DateUtil and StringUtil, like in Ext AS.

--- sencha-touch-as/src/main/joo-js/joo/net.jangaroo.sencha-touch-as-aliases.js
+++ sencha-touch-as/src/main/joo-js/joo/net.jangaroo.sencha-touch-as-aliases.js
@@ -2,6 +2,34 @@ ext = Ext;
 ext.Ext = Ext;
 ext.picker = Ext.Picker;
 ext.carousel = Ext.Carousel;
+ext.util.Observable.prototype.hasListener = (function(oldHasListener) {
+  return function(name) {
+    return this.events[name] && oldHasListener(name);
+  };
+})(ext.util.Observable.prototype.hasListener);
+ext.util.DateUtil = Date;
+// Forward all additional Date instance methods as static methods, since AS3 does not allow to extend a final class:
+ext.util.StringUtil = String;
+Ext.iterate({getTimezone: 0, getDayOfYear:0, getWeekOfYear:0, isLeapYear:0, getFirstDayOfMonth:0, getLastDayOfMonth:0,
+  getFirstDateOfMonth:0, getLastDateOfMonth:0, getDaysInMonth:0, getSuffix:0, clone:0, isDST:0,
+  getGMTOffset:1, clearTime:1, add:2, between:2, format:1},
+  function(method, arity) {
+    // We could do this in a general fashion, but that would mean using apply() combined with Array#slice(),
+    // which is more runtime overhead.
+    ext.util.DateUtil[method] =
+      arity === 0 ?
+        function(date) {
+          return date[method]();
+        }
+        : arity === 1 ?
+        function(date, arg1) {
+          return date[method](arg1);
+        }
+        : // arity can only be 2
+        function(date, arg1, arg2) {
+          return date[method](arg1, arg2);
+        };
+  });
 
 // ---- handle ExtConfig annotation ----
 joo.getOrCreatePackage("joo.meta").ExtConfig =
