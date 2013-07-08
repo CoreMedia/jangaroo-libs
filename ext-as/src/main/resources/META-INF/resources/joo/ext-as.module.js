a2041f70d9aec5a25bf6d8ef9cd943a9eaa41aac
Matthias Wester-Ebbinghaus <Matthias.Wester-Ebbinghaus@coremedia.com>
2013-07-08 17:45:39 +0200

    * Prepared to work with ext-js-3.4.1.1 * Prepared to work with jangaroo-tools 2.0.6

--- ext-as/src/main/resources/META-INF/resources/joo/ext-as.module.js
+++ ext-as/src/main/resources/META-INF/resources/joo/ext-as.module.js
@@ -1,6 +1,6 @@
-joo.loadScript("ext-js/adapter/ext/ext-base.js", "ext-js/adapter/ext/ext-base-debug.js");
-joo.loadScript("ext-js/ext-all.js", "ext-js/ext-all-debug.js");
-joo.loadDebugScript("ext-js/src/debug.js");
+joo.loadScript("public/com/sencha/ext-js/${extjs.version}/adapter/ext/ext-base.js", "public/com/sencha/ext-js/${extjs.version}/adapter/ext/ext-base-debug.js");
+joo.loadScript("public/com/sencha/ext-js/${extjs.version}/ext-all.js", "public/com/sencha/ext-js/${extjs.version}/ext-all-debug.js");
+joo.loadDebugScript("public/com/sencha/ext-js/${extjs.version}/src/debug.js");
 //joo.loadDebugScript("ext-js/src/error-checking.js");
 joo.loadScript("loadLocale.js");
 joo.loadScript("joo/net.jangaroo.ext-as-aliases.js");
