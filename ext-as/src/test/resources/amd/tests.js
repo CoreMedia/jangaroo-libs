41a010d117fc2ad57dd90d605b8cf2529884871b
Frank Wienberg <frank.wienberg@coremedia.com>
2015-02-20 10:39:51 +0100

    fix unit tests of ext-as: tests must require module config first!

--- ext-as/src/test/resources/amd/tests.js
+++ ext-as/src/test/resources/amd/tests.js
@@ -1,3 +1,5 @@
-require(["as3/net/jangaroo/joounit/runner/BrowserRunner", "as3/net/jangaroo/ext/TestSuite"], function(BrowserRunner, TestSuite) {
-  BrowserRunner._.main(TestSuite._);
+require(["lib/net/jangaroo/ext-as"], function() {
+  require(["as3/net/jangaroo/joounit/runner/BrowserRunner", "as3/net/jangaroo/ext/TestSuite"], function(BrowserRunner, TestSuite) {
+    BrowserRunner._.main(TestSuite._);
+  });
 });
