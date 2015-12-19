84ccd1f1984a13c84ea75ec284a015b551392681
Frank Wienberg <frank.wienberg@coremedia.com>
2015-12-20 00:36:10 +0100

    Much more compact test application declaration.

--- joounit/src/test/resources/tests.js
+++ joounit/src/test/resources/tests.js
@@ -1,29 +1,22 @@
-Ext.Loader.setPath('AS3', 'joo/AS3.js');
-Ext.Loader.setPath('joo', 'joo/classes/joo');
-Ext.Loader.setPath('com', 'joo/classes/com');
-Ext.Loader.setPath('net', 'joo/classes/net');
-Ext.Loader.setPath('flexunit', 'joo/classes/flexunit');
-Ext.Loader.setPath('flash', 'joo/classes/flash');
-Ext.Loader.setPath('mx', 'joo/classes/mx');
-
-Ext.define("FlExtAS.Application", {
-    extend: "Ext.app.Application",
-
-    requires: [
-        'AS3',
-        'net.jangaroo.joounit.runner.BrowserRunner',
-        'flexunit.framework.AllFrameworkTests'        
-    ],
-
-    launch: function() {
-      net.jangaroo.joounit.runner.BrowserRunner.main({
-        testSuite: flexunit.framework.AllFrameworkTests
-      });
-    }
-});
-
 Ext.application({
-    name: 'FlExtAS',
+  paths: {
+    'AS3': 'joo/AS3.js',
+    'joo': 'joo/classes/joo',
+    'com': 'joo/classes/com',
+    'net': 'joo/classes/net',
+    'flexunit': 'joo/classes/flexunit',
+    'flash': 'joo/classes/flash',
+    'mx': 'joo/classes/mx'
+  },
+  requires: [
+    'AS3',
+    'net.jangaroo.joounit.runner.BrowserRunner',
+    'flexunit.framework.AllFrameworkTests'
+  ],
 
-    extend: 'FlExtAS.Application',
+  launch: function() {
+    net.jangaroo.joounit.runner.BrowserRunner.main(
+            flexunit.framework.AllFrameworkTests
+    );
+  }
 });
