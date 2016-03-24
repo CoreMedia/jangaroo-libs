d26d3cef53cc2b80ffd4a61f5a53c0dcb2516684
Andreas Prantl <andreas.prantl@coremedia.com>
2016-03-24 16:40:26 +0100

    Removed ext-as local module and dependencies (Part 2)
    - removed sources as well

--- ext-as/src/main/resources/META-INF/resources/joo/ext-as.module.js
+++
@@ -1,7 +0,0 @@
-joo.loadScript("public/com/sencha/ext-js/${extjs.version}/adapter/ext/ext-base.js", "public/com/sencha/ext-js/${extjs.version}/adapter/ext/ext-base-debug.js");
-joo.loadScript("public/com/sencha/ext-js/${extjs.version}/ext-all.js", "public/com/sencha/ext-js/${extjs.version}/ext-all-debug.js");
-joo.loadDebugScript("public/com/sencha/ext-js/${extjs.version}/src/debug.js");
-//joo.loadDebugScript("ext-js/src/error-checking.js");
-joo.loadScript("loadLocale.js");
-joo.loadScript("joo/net.jangaroo.ext-as-aliases.js");
-joo.loadModule("net.jangaroo", "ext-as");
